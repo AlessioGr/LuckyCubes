89e996236d3b54bd489aac11986d54940151dcdc
ChickenStyle <sapunkovdanil@gmail.com>
2020-11-14 15:16:24 +0200

    1.0.4

--- LuckyCubes/src/me/chickenstyle/luckyblocks/events/PlayerStandManipulateEvent.java
+++ LuckyCubes/src/me/chickenstyle/luckyblocks/events/PlayerStandManipulateEvent.java
@@ -1,10 +1,9 @@
 package me.chickenstyle.luckyblocks.events;
 
 import org.bukkit.entity.ArmorStand;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerArmorStandManipulateEvent;
 
 import me.chickenstyle.luckyblocks.Main;
@@ -18,13 +17,10 @@ public class PlayerStandManipulateEvent implements Listener{
 	}
 	
 	@EventHandler
-	public void onPlayerBreak(EntityDamageByEntityEvent e) {
+	public void onPlayerBreak(EntityDamageEvent e) {
 		if (e.getEntity() instanceof ArmorStand) {
-			if (e.getDamager() instanceof Player) {
-				Player player = (Player) e.getDamager();
-				if (Main.opening.contains(player.getUniqueId())) {
-					e.setCancelled(true);
-				}
+			if (Main.stands.contains((ArmorStand) e.getEntity())) {
+				e.setCancelled(true);
 			}
 		}
 	}
